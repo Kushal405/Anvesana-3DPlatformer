e4dbef1f46ff29eaeaac5e646e10e0a39b958555
Kushal405 <kushalmaharjan0@gmai.com>
2026-04-28 18:13:00 +0545

    feat(zone3): add disappearing platforms and add stairs to jump.

---
+++ Assets/_Project/Scripts/Player/PlatformDisappear.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using DG.Tweening;
+
+public class DisappearPlatform : MonoBehaviour
+{
+    public float waitTime = 1.5f;
+    public float hideTime = 2f;
+    Renderer rend;
+    Collider col;
+    bool triggered = false;
+
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+        col  = GetComponent<Collider>();
+    }
+
+    void OnCollisionEnter(Collision c)
+    {
+        if (!triggered && c.gameObject.CompareTag("Player"))
+        {
+            triggered = true;
+            StartCoroutine(DisappearRoutine());
+        }
+    }
+
+    System.Collections.IEnumerator DisappearRoutine()
+    {
+        rend.material.DOColor(Color.red, waitTime * 0.5f);
+        yield return new WaitForSeconds(waitTime);
+
+        col.enabled = false;
+        rend.enabled = false;
+        yield return new WaitForSeconds(hideTime);
+
+        col.enabled = true;
+        rend.enabled = true;
+        rend.material.DOColor(Color.white, 0.3f);
+        triggered = false;
+    }
+}
